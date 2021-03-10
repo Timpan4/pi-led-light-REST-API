2c696a0b4342eb2b23b9f4251697d4859fd01c57
Tim Aronsson <70634109+Timpan4@users.noreply.github.com>
2021-03-10 18:18:17 +0100

    fix

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 var express = require("express");
+
+const Gpio = require('pigpio').Gpio;
 var app = express();
+
+
+
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
+});
+app.post("/update", (req, res, next) => {
+    let rPin = new Gpio(req.rPin, { mode: Gpio.OUTPUT });;
+    let gPin = new Gpio(req.gPin, { mode: Gpio.OUTPUT });;
+    let bPin = new Gpio(req.pPin, { mode: Gpio.OUTPUT });;
+    rPin.pwmWrite(req.red);
+    gPin.pwmWrite(req.green);
+    bPin.pwmWrite(req.blue);
 });

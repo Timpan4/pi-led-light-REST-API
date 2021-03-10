5f119091aefffc66b2caa196443353d411ae60b8
Tim Aronsson <70634109+Timpan4@users.noreply.github.com>
2021-03-10 19:07:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require("express");
+const bodyParser = require("body-parser");
 
 const Gpio = require('pigpio').Gpio;
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 
 
 
@@ -11,13 +16,13 @@ app.post("/update", (req, res) => {
     let rPin = new Gpio(req.rPin, { mode: Gpio.OUTPUT });;
     let gPin = new Gpio(req.gPin, { mode: Gpio.OUTPUT });;
     let bPin = new Gpio(req.pPin, { mode: Gpio.OUTPUT });;
-    console.log(req);
-    rPin.pwmWrite(req.red);
-    gPin.pwmWrite(req.green);
-    bPin.pwmWrite(req.blue);
+    console.log(req.body);
+    rPin.pwmWrite(req.body.red);
+    gPin.pwmWrite(req.body.green);
+    bPin.pwmWrite(req.body.blue);
     res.sendStatus("hello i have recieve");
 });
-
+app.use("/", router);
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });

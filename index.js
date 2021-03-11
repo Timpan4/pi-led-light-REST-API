eda8db687c6800a4fe914b2818995a01632a7882
Tim Aronsson <70634109+Timpan4@users.noreply.github.com>
2021-03-11 02:52:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // });
 
 app.post('/update', function (req, res) {
-    console.log(req.body);
+    // console.log(req.body);
     // res.set('Content-Type', 'text/plain')
-    res.send("hello i have recieve");
-    let rPin = new Gpio(req.body.rPin, { mode: Gpio.OUTPUT });
-    let gPin = new Gpio(req.body.gPin, { mode: Gpio.OUTPUT });
-    let bPin = new Gpio(req.body.pPin, { mode: Gpio.OUTPUT });
-    rPin.pwmWrite(req.body.red);
-    gPin.pwmWrite(req.body.green);
-    bPin.pwmWrite(req.body.blue);
+    res.send(json.stringify(req.body));
+    // let rPin = new Gpio(req.body.rPin, { mode: Gpio.OUTPUT });
+    // let gPin = new Gpio(req.body.gPin, { mode: Gpio.OUTPUT });
+    // let bPin = new Gpio(req.body.pPin, { mode: Gpio.OUTPUT });
+    // rPin.pwmWrite(req.body.red);
+    // gPin.pwmWrite(req.body.green);
+    // bPin.pwmWrite(req.body.blue);
 });
 
 app.listen(port, (err) => {

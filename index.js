3f08ba6866c0c247f8fb7a15c02f89f4caa53d4b
Tim Aronsson <70634109+Timpan4@users.noreply.github.com>
2021-03-11 01:31:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,25 @@ const bodyParser = require("body-parser");
 const Gpio = require('pigpio').Gpio;
 const router = express.Router();
 var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-app.post("/update", (req, res) => {
-    console.log(req.body);
-    let rPin = new Gpio(req.rPin, { mode: Gpio.OUTPUT });;
-    let gPin = new Gpio(req.gPin, { mode: Gpio.OUTPUT });;
-    let bPin = new Gpio(req.pPin, { mode: Gpio.OUTPUT });;
-    rPin.pwmWrite(req.body.red);
-    gPin.pwmWrite(req.body.green);
-    bPin.pwmWrite(req.body.blue);
-    res.sendStatus("hello i have recieve");
-});
-app.use("/", router);
 
 
 app.listen(3000, () => {
     let rPin = new Gpio(5, { mode: Gpio.OUTPUT });;
-    rPin.pwmWrite(0);
+    rPin.pwmWrite(255);
     console.log("Server running on port 3000");
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+
+    app.post("/update", (req, res) => {
+        console.log(req.body);
+        let rPin = new Gpio(req.rPin, { mode: Gpio.OUTPUT });;
+        let gPin = new Gpio(req.gPin, { mode: Gpio.OUTPUT });;
+        let bPin = new Gpio(req.pPin, { mode: Gpio.OUTPUT });;
+        rPin.pwmWrite(req.body.red);
+        gPin.pwmWrite(req.body.green);
+        bPin.pwmWrite(req.body.blue);
+        res.sendStatus("hello i have recieve");
+    });
+    app.use("/", router);
 });

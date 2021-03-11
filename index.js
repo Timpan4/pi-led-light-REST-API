4d81ee4cb0991ed68ba5065bd876a4a32d92f35d
Tim Aronsson <70634109+Timpan4@users.noreply.github.com>
2021-03-11 03:15:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ app.post('/update', function (req, res) {
     // console.log(req.body);
     // res.set('Content-Type', 'text/plain')
     res.send(JSON.stringify(req.body));
-    let rPin = new Gpio(Int(req.body.rPin), { mode: Gpio.OUTPUT });
-    let gPin = new Gpio(Int(req.body.gPin), { mode: Gpio.OUTPUT });
-    let bPin = new Gpio(Int(req.body.pPin), { mode: Gpio.OUTPUT });
+    let rPin = new Gpio(Number(req.body.rPin), { mode: Gpio.OUTPUT });
+    let gPin = new Gpio(Number(req.body.gPin), { mode: Gpio.OUTPUT });
+    let bPin = new Gpio(Number(req.body.pPin), { mode: Gpio.OUTPUT });
     rPin.pwmWrite(req.body.red);
     gPin.pwmWrite(req.body.green);
     bPin.pwmWrite(req.body.blue);

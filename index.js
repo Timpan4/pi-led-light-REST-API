39e6cba91211e988fe7479e1d19207e2cfc01fae
Tim Aronsson <70634109+Timpan4@users.noreply.github.com>
2021-03-11 10:35:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,10 +19,20 @@ app.post('/update', function (req, res) {
     // console.log(req.body);
     // res.set('Content-Type', 'text/plain')
     res.send(JSON.stringify(req.body));
-    let rPin = new Gpio(5, { mode: Gpio.OUTPUT });
+    let pin = parseInt(req.body.rPin);
+
+    let rPin = new Gpio(pin, { mode: Gpio.OUTPUT });
     // let gPin = new Gpio(String(req.body.gPin), { mode: Gpio.OUTPUT });
     // let bPin = new Gpio(String(req.body.pPin), { mode: Gpio.OUTPUT });
+
     rPin.pwmWrite(req.body.red);
+    for (let tempBrightness = 0; tempBrightness <= brightness; tempBrightness++) {
+        sleep(5);
+    }
+    while (brightness != 0) {
+        brightness--;
+        sleep(5);
+    }
     // gPin.pwmWrite(req.body.green);
     // bPin.pwmWrite(req.body.blue);
 });
@@ -45,3 +55,34 @@ app.listen(port, (err) => {
 //     });
 //     app.use("/", router);
 // });
+
+function HSLToRGB(h, s, l) {
+
+    if (0 <= h && h < 60) {
+        r = c; g = x; b = 0;
+    } else if (60 <= h && h < 120) {
+        r = x; g = c; b = 0;
+    } else if (120 <= h && h < 180) {
+        r = 0; g = c; b = x;
+    } else if (180 <= h && h < 240) {
+        r = 0; g = x; b = c;
+    } else if (240 <= h && h < 300) {
+        r = x; g = 0; b = c;
+    } else if (300 <= h && h < 360) {
+        r = c; g = 0; b = x;
+    }
+    r = Math.round((r + m) * 255);
+    g = Math.round((g + m) * 255);
+    b = Math.round((b + m) * 255);
+
+    return [r, g, b];
+}
+//sleep function
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
+}

891be71db4ab5f6cf9a5b818a124be081d9121b3
Tim Aronsson <70634109+Timpan4@users.noreply.github.com>
2021-03-11 02:27:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const router = express.Router();
-const app = express();
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+// Create a new instance of express
+const app = express()
 const Gpio = require('pigpio').Gpio;
 const port = 80;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-router.get('/', (req, res) => {
-    res.sendfile("index.html");
-});
+// router.get('/', (req, res) => {
+//     res.sendfile("index.html");
+// });
 
-router.get('/update', (req, res) => {
+app.get('/update', (req, res) => {
     console.log(req.body);
     let rPin = new Gpio(req.body.rPin, { mode: Gpio.OUTPUT });;
     let gPin = new Gpio(req.body.gPin, { mode: Gpio.OUTPUT });;
@@ -24,8 +27,11 @@ router.get('/update', (req, res) => {
     res.send("hello i have recieve");
 });
 
-app.listen(port, () => {
+app.listen(port, (err) => {
     console.log("Started on PORT " + port);
+    if (err) {
+        throw err;
+    }
 })
 // app.listen(3000, () => {
 //     let rPin = new Gpio(5, { mode: Gpio.OUTPUT });;
